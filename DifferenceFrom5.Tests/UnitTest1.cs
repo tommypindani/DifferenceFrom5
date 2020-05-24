2b35bf8e83ba94a6a8b7494157c6d620ea96978d
Brendon Pindani <TommyPindani@gmail.com>
2020-05-24 19:12:46 +0200

    push changes

--- DifferenceFrom5.Tests/UnitTest1.cs
+++ DifferenceFrom5.Tests/UnitTest1.cs
@@ -15,36 +15,23 @@ namespace DifferenceFrom5.Tests
         [TestMethod]
         public void IfInputIs4Return1()
         {
-            var differenceFrom5 = NumberUtility.CalculateDifferenceFrom5(2);
-            Assert.AreEqual(3, differenceFrom5);
-        }
-
-        [TestMethod]
-        public void IfInputIs3Return2()
-        {
-            var differenceFrom5 = NumberUtility.CalculateDifferenceFrom5(3);
-            Assert.AreEqual(2, differenceFrom5);
-        }
-
-        [TestMethod]
-        public void IfInputIs2Return3()
-        {
             var differenceFrom5 = NumberUtility.CalculateDifferenceFrom5(4);
             Assert.AreEqual(1, differenceFrom5);
         }
-
         [TestMethod]
-        public void IfInputIs4Return4()
+        public void DifferenceFrom5_IsValidNumber()
         {
-            var differenceFrom5 = NumberUtility.CalculateDifferenceFrom5(5);
-            Assert.AreEqual(0, differenceFrom5);
+            var differenceFrom5 = 0 < 5;
+            Assert.IsTrue(0 < 5);
         }
 
+
         [TestMethod]
-        public void IfInputIs4Return5()
+        public void DifferenceFrom5_IsInvalidNumber()
         {
-            var differenceFrom5 = NumberUtility.CalculateDifferenceFrom5(2);
-            Assert.AreEqual(3, differenceFrom5);
+            var differenceFrom5 = false;
+
+            Assert.ThrowsException<System.ApplicationException>( DifferenceFrom5_IsInvalidNumber);
         }
     }
 }
